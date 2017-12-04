c9339281827b65560798d106093bd5194cd0e6ad
Kyziur <kyziurs@gmail.com>
2017-12-04 22:43:27 +0100

    Cleaning

--- README.md
+++
@@ -1,1 +0,0 @@
-Simple website project made in college. 
--- config/css.php
+++
@@ -1,9 +0,0 @@
-<!--- Bootstrap CSS -->
-<link href="css/bootstrap.min.css" rel="stylesheet">
-<link href="css/starter-template.css" rel="stylesheet">
-<link href="css/signin.css" rel="stylesheet">
-
-
-<!---jQueryUI CSS-->
-<link href="css/jquery-ui.min.css" rel="stylesheet">
-
